e67ef596444d0a9a609f9ea448bcf52c7feadf40
RaissaKestring <raissakestring23@gmail.com>
2024-05-20 17:27:59 -0300

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([
-            UsersSeeder::class,
-        ]);
+        $this->call(CategoriasSeeder::class);
     }
 }
