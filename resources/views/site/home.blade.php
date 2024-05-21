58683f66e0065662afa6efba8f17047f1cdae8f2
RaissaKestring <raissakestring23@gmail.com>
2024-05-21 17:15:11 -0300

    Stacks e Push

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,5 +1,27 @@
 @extends('site.layout')
 @section('title', 'Essa é a página HOME')
 @section('conteudo')
-    <h1>Essa é nossa home</h1>
+
+@include('includes.mensagem', ['titulo' => 'Mensagem de sucesso!'])
+
+@component('components.sidebar')
+    @slot('paragrafo')
+        Texto qualquer vindo do slot
+    @endslot
+@endcomponent
+
 @endsection
+
+@push('style')
+
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+@endpush
+
+@push('script')
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+@endpush
