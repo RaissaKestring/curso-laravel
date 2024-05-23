4f0a0772a535e68e5ed33e53aecb35dc6a15f8e6
RaissaKestring <raissakestring23@gmail.com>
2024-05-23 14:54:11 -0300

    Estilizando carrinho

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -3,21 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Darryldecode\Cart\Facades\CartFacade;
 class CarrinhoController extends Controller
 {
     public function carrinhoLista() {
-        $itens = \Cart::getContent();
-        dd($itens);
+        $itens = CartFacade::getContent();
+        return view('site.carrinho', compact('itens'));
     }
 
     public function adicionaCarrinho(Request $request) {
-        \Cart::add([
+        CartFacade::add([
             'id' => $request->id,
             'name' => $request->name,
             'price' => $request->price,
             'quantity' => $request->qnt,
             'attributes' => array(
-                'image' ->$request->img
+                'image' => $request->img
             )
         ]);
     }
