26bed19f9d452f12877d7591a3f477cbc502fba2
RaissaKestring <raissakestring23@gmail.com>
2024-05-27 17:37:37 -0300

    Gráficos com dados dinâmicos

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use App\Models\Categoria;
+use App\Models\Produto;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('admin.dashboard');
+        $usuarios = User::all()->count();
+
+        // grafico 1 - usuarios
+        $usersData = User::select([
+            DB::raw('YEAR(created_at) as ano'),
+            DB::raw('COUNT(*) as total')
+        ])
+        ->groupBy('ano')
+        ->orderBy('ano', 'asc')
+        ->get();
+        // preparar arrays
+
+        foreach ($usersData as $user) {
+            $ano[] = $user -> ano;
+            $total[] = $user -> total;
+
+            // formatar para chatsjs
+            $userLabel = "'Comparativo de cadastros de usuários'";
+            $userAno = implode(',', $ano);
+            $userTotal = implode(',', $total);
+
+            // grafico 2 - categorias
+            $catData = Categoria::all();
+
+            // preparar array
+            foreach ($catData as $cat) {
+                $catNome[] = "'".$cat -> name;
+                $catTotal[] = Produto::where("id_categoria", $cat->id)->count();
+            }
+            
+
+            // formatar para chartjs
+            $catLabel = implode(',', $catNome);
+            $catLabel = implode(',', $catTotal);
+
+        return view('admin.dashboard', compact('usuarios', 'userLabel', 'userAno', 'userTotal', 'catLabel', 'catTotal'));
     }
+}
 }
